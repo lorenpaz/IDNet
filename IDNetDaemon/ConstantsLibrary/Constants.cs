4dbc008e20d7af16647063616fd1081ab7388000
lorenpaz <lorenpaz@ucm.es>
2017-10-24 16:33:15 +0200

    Probando una subida modificada

--- IDNetDaemon/ConstantsLibrary/Constants.cs
+++ IDNetDaemon/ConstantsLibrary/Constants.cs
@@ -6,10 +6,25 @@ namespace ConstantsLibrary
         public static string MONGODB = "mongodb";
         public static string MYSQL = "mysql";
         public static string TEMPORAL_FILE_PATH = "./temp_files/";
+		public static string CONEXION = "001";
+		public static string SCHEMA = "002";
+		public static string SELECT = "003";
+		public static string ACKCONEXION = "004";
+		public static string ACKSCHEMA = "005";
+		public static string ACKSELECT = "006";
     }
 
     public enum DatabasesEnum {
         MONGODB,
         MYSQL
     }
+	public enum typesMessages
+	{
+		CONEXION,
+		SCHEMA,
+		SELECT,
+		ACKCONEXION,
+		ACKSCHEMA,
+		ACKSELECT
+	}
 }
